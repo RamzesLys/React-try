be7a422cc90baf2bad2aad8b2239f22dfef1f452
Ramzes <ramzes311982@gmail.com>
2023-03-09 10:26:55 +0000

    Update 3 files

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const node = document.getElementById("root");
 
+React.createElement(
+    String/ReactClass type,
+    [object props],
+    [children...]
+) -> React Element
 // ReactDOM.render (
 //   ReactElement element,
 //   DOMElement container,
